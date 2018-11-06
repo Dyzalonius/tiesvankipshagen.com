7805998a668fc6fa737890c3b5dd3e9a0b0734f1
dyzalonius <dyzalonius@gmail.com>
2018-11-06 19:04:24 +0100

    Added selector to gallery

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
 
     $(".galleryItem").click(function () {
-        var source = $(this).attr('src');
-        var itemLeft = $(this).parent().parent();
-        var image = itemLeft.find('.galleryDisplay');
+        // remove old current
+        var galleryWrapper = $(this).parent();
+        $(galleryWrapper).children().removeClass("current");
 
+        // set new current
+        $(this).addClass("current");
+
+        // set source of display
+        var source = $(this).attr("src");
+        var itemLeft = $(this).parent().parent();
+        var image = itemLeft.find(".galleryDisplay");
         $(image).attr("src", source);
     });
 });
