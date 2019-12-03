21b6c355cf8dbd456ff7f02aeb87a0efa4ec7f18
dyzalonius <dyzalonius@gmail.com>
2019-12-03 14:27:53 +0100

    Add galleryItemVideo support to have any combination of videos and images per project.

--- public/assets/js/gallery.js
+++ public/assets/js/gallery.js
@@ -1,17 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function ()
+{
+    $(".galleryItem").click(function ()
+    {
+        // exit if already current
+        if ($(this).hasClass("current"))
+            return;
 
-    $(".galleryItem").click(function () {
-        // remove old current
-        var galleryWrapper = $(this).parent();
-        $(galleryWrapper).children().removeClass("current");
-
-        // set new current
+        // remove old current and set new current
+        $(this).parent().children().removeClass("current");
         $(this).addClass("current");
 
-        // set source of display
-        var source = $(this).attr("src");
-        var itemLeft = $(this).parent().parent();
-        var image = itemLeft.find(".galleryDisplay");
-        $(image).attr("src", source);
+        // set display
+        var isVideo = $(this).hasClass("galleryItemVideo");
+        if (isVideo)
+        {
+            // video
+            var source = $(this).attr("videoSource");
+            var gallery = $(this).parent().parent();
+            var galleryMain = gallery.find(".galleryMain");
+            $(galleryMain).html("<div class='galleryVideoWrapper'><iframe class='galleryVideo' allowfullscreen></iframe></div>");
+            var galleryVideo = galleryMain.find(".galleryVideo");
+            galleryVideo.attr("src", source);
+        }
+        else
+        {
+            // image
+            var source = $(this).attr("src");
+            var gallery = $(this).parent().parent();
+            var galleryMain = gallery.find(".galleryMain");
+            $(galleryMain).html("<img class='galleryDisplay' alt='Screenshot'>");
+            var galleryImage = galleryMain.find(".galleryDisplay");
+            $(galleryImage).attr("src", source);
+        }
     });
 });
