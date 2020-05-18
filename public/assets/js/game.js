2d302a6c3e4251d83ecea3e3322d359a5468901d
dyzalonius <dyzalonius@gmail.com>
2020-05-19 00:25:52 +0200

    Re-add targets

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -55,11 +55,11 @@ function Start() {
 }
 
 function StartGame() {
-    //new ShootingTarget({x:700, y:200}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
-    //new ShootingTarget({x:1300, y:700}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
-    //new ShootingTarget({x:1400, y:300}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
-    //new Rocket({x:1000, y:500}, 270, turret, 500, 120, {x:50, y:50}, 10, 'blue', './assets/img/game/missile.png');
-    //new Bomb({x:350, y:200}, 270, 300, 180, {x:50, y:50}, 10, 'black', './assets/img/game/bomb.png');
+    new ShootingTarget({x:700, y:200}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
+    new ShootingTarget({x:1300, y:700}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
+    new ShootingTarget({x:1400, y:300}, {x:100, y:100}, 25, 'red', 'rgb(230,230,230)');
+    new Rocket({x:1000, y:500}, 270, turret, 500, 120, {x:50, y:50}, 10, 'blue', './assets/img/game/missile.png');
+    new Bomb({x:350, y:200}, 270, 300, 180, {x:50, y:50}, 10, 'black', './assets/img/game/bomb.png');
 }
 
 function Update(timeStamp) {
